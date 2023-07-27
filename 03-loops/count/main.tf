8069666493af04b625a982b1e186e5b4c3371a7f
akr9757 <anilkumareddy9757@gmail.com>
2023-07-27 09:38:32 +0530

    server creation

--- 03-loops/count/main.tf
+++ 03-loops/count/main.tf
@@ -1,8 +1,8 @@
-resource "null_resource" "null" {}
-
-resource "null_resource" "null" {
-  count = 10
-}
+#resource "null_resource" "null" {}
+#
+#resource "null_resource" "null" {
+#  count = 10
+#}
 
 resource "null_resource" "fruits" {
   count = length(var.friuts_names)
