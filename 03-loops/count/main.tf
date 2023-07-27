ff60636cc9dfbd2f2bd6660a3020575fc6d01f49
akr9757 <anilkumareddy9757@gmail.com>
2023-07-27 09:50:41 +0530

    server creation

--- 03-loops/count/main.tf
+++ 03-loops/count/main.tf
@@ -1,5 +1,16 @@
+resource "null_resource" "null" {}
+
+resource "null_resource" "null" {
+  count = 10
+}
+
 resource "null_resource" "fruits" {
   count = length(var.fruits)
+
+  provisioner "local-exec" {
+    command = "echo fruit name - ${var.fruits[count.index]}"
+  }
+
 }
 
 variable "fruits" {
