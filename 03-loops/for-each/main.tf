9ada33b93ca34172468f9ce837000d1b82bac30c
akr9757 <anilkumareddy9757@gmail.com>
2023-07-27 13:49:36 +0530

    server creation

---
+++ 03-loops/for-each/main.tf
@@ -0,0 +1,17 @@
+resource "null_resource" "fruits" {
+  for_each = var.fruits
+
+  provisioner "local-exec" {
+    command = "echo fruit_name ${each.key} - ${each.value}"
+  }
+}
+
+
+
+variable "fruits" {
+  default = {
+    apple = 10
+    banana =100
+    grapes = 50
+  }
+}
