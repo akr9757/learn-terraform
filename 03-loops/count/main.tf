4f385dac622d4e9c8f5b7deac0234b700ea4ec98
akr9757 <anilkumareddy9757@gmail.com>
2023-07-27 09:37:00 +0530

    server creation

---
+++ 03-loops/count/main.tf
@@ -0,0 +1,13 @@
+resource "null_resource" "null" {}
+
+resource "null_resource" "null" {
+  count = 10
+}
+
+resource "null_resource" "fruits" {
+  count = length(var.friuts_names)
+}
+
+variable "fruits_names" {
+  default = [ "banana", "apple", "grapes" ]
+}
