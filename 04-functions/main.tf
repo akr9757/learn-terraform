8e90be81c4f55327a3f459ad89135709ee6905a8
akr9757 <anilkumareddy9757@gmail.com>
2023-07-28 16:39:20 +0530

    server creation

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -10,3 +10,11 @@ output "call" {
 output "call1" {
   value = upper(var.sample)
 }
+
+variable "sample2" {
+  default = [ "abc", "xyz" ]
+}
+
+output "call4" {
+  value = element(var.sample2, 0)
+}
