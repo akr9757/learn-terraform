3f0404a72ad64989797cf6cbabb5c82b0d62e573
akr9757 <anilkumareddy9757@gmail.com>
2024-08-10 14:05:07 +0530

    terraform code

--- expense/variables.tf
+++ expense/variables.tf
@@ -5,3 +5,27 @@ variable "fruits" {
 variable "fruits1" {
   default = [ "apple", "mango", "banana"]
 }
+
+variable "actors" {
+  default = {
+    mahesh = {
+      name = "mahesh"
+      height = 6
+    }
+    prabhas = {
+      name = "prabhas"
+      height = 7
+    }
+    allu = {
+      name = "allu"
+      height = 5
+    }
+  }
+}
+
+resource "null_resource" "actors" {
+  for_each = var.actors
+
+  provisioner "local-exec" {
+    command = "echo actor name is ${each.value["name"]} and his height is ${each.value["height"]}"
+}
